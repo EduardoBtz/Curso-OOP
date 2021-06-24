65994cad510d03dab66b88e883f46477d59b30f2
EduardoBtz <eduardombc96@gmail.com>
2021-06-24 16:21:18 -0500

    Added instances from classes in java and javascript scripts.

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hello world!")
+    car = Car()
+    car.license = "AMS1233"
+    car.driver = "Jesus Orjle"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "JJK123"
+    car2.driver = "Carlos Bella"
+    print(vars(car2))
+   
